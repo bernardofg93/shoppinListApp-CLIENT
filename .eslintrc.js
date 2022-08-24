aadc7714644167c4213b31e3a02dfc81c8564e14
Bernardo <bernardo930321@gmail.com>
2022-08-24 08:47:57 -0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "strict": 0,
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off",
     "react/jsx-uses-react": "error",
