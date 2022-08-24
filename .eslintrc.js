09eea01ff04140092945eabdf277b173009eeed4
Bernardo <bernardo930321@gmail.com>
2022-08-24 08:42:01 -0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@next/next/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        'plugin:@next/next/recommended',
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "jsx": true,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/no-unescaped-entities": "off",
-        "@next/next/no-page-custom-font": "off"
-    }
-}
+  },
+  plugins: ["react"],
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+  },
+};
